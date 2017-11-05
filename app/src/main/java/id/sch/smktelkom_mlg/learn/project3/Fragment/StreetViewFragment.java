70a7cedc4b4df2a2959797c3ed78eb5bed638191
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2017-11-05 12:20:09 +0700

    AddFragmentForMap

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/project3/Fragment/StreetViewFragment.java
@@ -0,0 +1,30 @@
+package id.sch.smktelkom_mlg.learn.project3.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import id.sch.smktelkom_mlg.learn.project3.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class StreetViewFragment extends Fragment {
+
+
+    public StreetViewFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_street_view, container, false);
+    }
+
+}
